81c246ca805b6a52f5d2fd716f2932edda8fb79a
Nana Kwame <iabu007@st.ug.edu.gh>
2023-07-14 15:16:26 +0000

    updated resources pages and added Courses component

--- app/resources/courses/page.jsx
+++ app/resources/courses/page.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ResourceTopSection from "@components/ResourceTopSection";
 
+import "@styles/courses.css";
+import CoursesComp from "@components/CoursesComp";
+
 const CoursesPage = () => {
   return (
     <>
-      <ResourceTopSection text="this course pages" /> CoursesPage
+      <ResourceTopSection text="this course pages" CActiveStyle="c-active" />
+      <CoursesComp />
     </>
   );
 };
