92ca6b329e0045e8b47c2b3d8168ff7507a51a2a
Nana Kwame <iabu007@st.ug.edu.gh>
2023-07-21 16:18:13 +0000

    commit

--- components/MediaHeader.jsx
+++ components/MediaHeader.jsx
@@ -4,11 +4,8 @@ import Link from "next/link";
 import "@styles/media/header.css";
 
 const MediaHeader = (props) => {
-<<<<<<< HEAD
   const { EActiveStyle, PActiveStyle, aActiveStyle } = props;
-=======
-  const { EActiveStyle, PActiveStyle, aActiveStyle} = props;
->>>>>>> dffa54a5def441c8a8c9d555b52822c77edf77ef
+
   return (
     <div className="media-header-container">
       <div className="media-header">
@@ -19,11 +16,7 @@ const MediaHeader = (props) => {
           <Link href="/media/picnic_images">Picnic </Link>
         </span>
         <span className={aActiveStyle}>
-<<<<<<< HEAD
-          <Link href="">Ant </Link>
-=======
           <Link href="">Picnic </Link>
->>>>>>> dffa54a5def441c8a8c9d555b52822c77edf77ef
         </span>
       </div>
     </div>
